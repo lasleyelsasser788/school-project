11fe991c82f255a1e6e90c544be8f860b96b4f3d
lasleyelsasser788 <lasleyelsasser788@gmail.com>
2025-03-29 20:01:27 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,14 @@
+public class SchoolProjectJava {
+    public static void main(String[] args) {
+        // Example Java program
+        System.out.println("Hello, World!");
+        int x = 5;
+        double y = 3.14;
+        String name = "Alice";
+        
+        // Printing the values of variables
+        System.out.print(x + ", ");
+        System.out.print(y + ", ");
+        System.out.print(name);
+    }
+}
