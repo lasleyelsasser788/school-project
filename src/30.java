8599a35d7a1dca274e7fb6eccc5a7be3102422c7
lasleyelsasser788 <lasleyelsasser788@gmail.com>
2025-04-24 15:37:05 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,10 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+        // Example of simple program with basic operations
+        int num1 = 5;
+        int num2 = 3;
+        int result = num1 + num2; // Result: 8
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + result);
+    }
+}
