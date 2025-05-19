eca55dab439a4224900bd36b156a473f2039d758
lasleyelsasser788 <lasleyelsasser788@gmail.com>
2025-05-19 22:39:17 +0000

    Add 36.java

---
+++ src/36.java
@@ -0,0 +1,21 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+        // Example 1: Add two numbers
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Example 2: Subtract one number from another
+        int difference = 8 - 4;
+        System.out.println("The difference between " + 8 - 4 + " and 3 is: " + difference);
+
+        // Example 3: Multiply two numbers
+        double product = 7.0 * 6.5;
+        System.out.println("The product of 7.0 and 6.5 is: " + product);
+
+        // Example 4: Divide one number by another
+        int quotient = 12 / 3;
+        System.out.println("The quotient of 12 divided by 3 is: " + quotient);
+    }
+}
